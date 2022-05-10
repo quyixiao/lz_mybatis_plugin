2e2c797be60eb75f906f18e7cab786bed10b70fd
quyixiao <2621048238@qq.com>
2022-05-10 14:42:27 +0800

    添加时间

---
+++ src/main/java/com/lz/mybatis/plugin/utils/DF_Const.java
@@ -0,0 +1,11 @@
+package com.lz.mybatis.plugin.utils;
+
+public class DF_Const {
+
+    public static final String y = "%Y";
+    public static final String y_m = "%Y-%m";
+    public static final String y_m_d = "%Y-%m-%d";
+    public static final String y_m_d_h = "%Y-%m-%d %H";
+    public static final String y_m_d_h_i = "%Y-%m-%d %H:%i";
+    public static final String y_m_d_h_i_s = "%Y-%m-%d %H:%i:%S";
+}
